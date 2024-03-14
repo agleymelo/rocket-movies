7e7b56d71ae084d3979c08275d875b0781a85a9a
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-14 19:24:09 -0300

    SignIn Integration with api

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import theme from './styles/theme.js'
 import GlobalStyles from './styles/global.js'
 
 import { Routes } from './routes/index.routes.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <AuthProvider>
+        <GlobalStyles />
 
-      <Routes />
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { FiMail, FiLock } from 'react-icons/fi'
 
 import { Input } from '../../component/Input'
 import { Button } from '../../component/Button'
+import { useAuth } from '../../hooks/useAuth'
 
 import * as S from './styles'
-import { Link } from 'react-router-dom'
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  async function handleSignIn() {
+    if (!email || !password) {
+      alert('preencha os campos para realizar o login')
+    }
+
+    signIn({ email, password })
+  }
+
   return (
     <S.Container>
       <S.Form>
@@ -16,10 +31,15 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input icon={FiMail} placeholder="E-mail" type="email" />
-        <Input icon={FiLock} placeholder="Senha" type="password" />
+        <Input icon={FiMail} placeholder="E-mail" type="email" onChange={(event) => setEmail(event.target.value)} />
+        <Input
+          icon={FiLock}
+          placeholder="Senha"
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar Conta</Link>
       </S.Form>
